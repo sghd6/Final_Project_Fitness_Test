4884c8cd58cfd0472c4092beb47e1776de18edf5
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-05-02 17:20:22 -0500

    Add files via upload

---
+++ Final_Project_Fitness_Test/src/program/main/WorkoutStrategies/IntenseStrategy.java
@@ -0,0 +1,16 @@
+package program.main.WorkoutStrategies;
+
+import program.main.UserClasses.AbstractUser;
+
+public class IntenseStrategy implements IWorkoutStrategy{
+
+	
+	private final static double intenseMultiplier = 1.5;
+	
+	@Override
+	public void choosePlan(AbstractUser user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Final_Project_Fitness_Test/src/program/main/WorkoutStrategies/RegularStrategy.java
@@ -0,0 +1,15 @@
+package program.main.WorkoutStrategies;
+
+import program.main.UserClasses.AbstractUser;
+
+public class RegularStrategy implements IWorkoutStrategy{
+
+	
+	private final static double regularMultiplier = 1;
+	@Override
+	public void choosePlan(AbstractUser user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
