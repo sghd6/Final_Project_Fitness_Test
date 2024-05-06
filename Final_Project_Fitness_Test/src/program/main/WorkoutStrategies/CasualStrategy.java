9a3c6a3f81e0a31bccdeb5861a65d386227b3995
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-05-05 23:02:06 -0500

    Update CasualStrategy.java
    Small update to allow method to affect the user class.

--- Final_Project_Fitness_Test/src/program/main/WorkoutStrategies/CasualStrategy.java
+++ Final_Project_Fitness_Test/src/program/main/WorkoutStrategies/CasualStrategy.java
@@ -7,9 +7,15 @@ public class CasualStrategy implements IWorkoutStrategy{
 	
 	private final static double casualMultiplier = 0.5;
 	
+	
 	@Override
-	public void choosePlan(AbstractUser user) {
-		// TODO Auto-generated method stub
+	public boolean choosePlan(AbstractUser user) {
+		if (user != null)
+		{
+			user.setTestValue(casualMultiplier);
+			return true;
+		}
+		return false;
 		
 	}
 
