7f86cb01873056730aa91df1afb666e15e2b16a3
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-05-06 18:38:22 -0500

    updated Main
    Created place holder and methods for my user input options.

--- Final_Project_Fitness_Test/src/program/main/Main.java
+++ Final_Project_Fitness_Test/src/program/main/Main.java
@@ -4,20 +4,29 @@ import java.io.*;
 import java.util.*;
 import program.main.FitnessTracker.GoalType;
 import program.main.FitnessTracker.FitnessTracker;
+import program.main.DailyTips.DailyTips;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static FitnessTracker tracker = new FitnessTracker();
 
     public static void main(String[] args) {
+    	
         tracker.loadData(); // Load any previously saved user goals
+        System.out.println();
         System.out.println("Hi! Welcome to the Fitness Test!");
+        
+        System.out.println();
+        DailyTips dailyTips = new DailyTips();
+        dailyTips.displayDailyTip();
+        System.out.println();
+        
 
         boolean runApplication = true;
         while (runApplication) {
             System.out.println("\nPlease choose one of the following options:");
             System.out.println("1. Login/Create User and Manage Goals");
-            System.out.println("2. Exit");
+            System.out.println("3. Exit");
 
             int mainOption = scanner.nextInt();
             scanner.nextLine(); // Consume the newline
@@ -60,7 +69,11 @@ public class Main {
             System.out.println("1. Create a new goal");
             System.out.println("2. View my goals");
             System.out.println("3. Update a goal");
-            System.out.println("4. Return to main menu");
+            System.out.println("4. Track an activiy");
+            System.out.println("5. Generate a weekly activity summary");
+            System.out.println("6. Calculate Calories Burn");
+            System.out.println("7. Track Water Intake");
+            System.out.println("8. Return to main menu");
 
             int userChoice = scanner.nextInt();
             scanner.nextLine(); // Consume the newline
@@ -76,6 +89,18 @@ public class Main {
                     updateGoal(userName);
                     break;
                 case 4:
+                    trackActivity(userName);
+                    break;
+                case 5:
+                    generateSummary(userName);
+                    break;
+                case 6:
+                    caloriesBurned(userName);
+                    break;
+                case 7:
+                	waterIntake(userName);
+                	break;
+                case 8:
                     return; // Return to the main menu
                 default:
                     System.out.println("Invalid option. Please select 1, 2, 3, or 4.");
@@ -83,7 +108,7 @@ public class Main {
         }
     }
 
-    private static void createGoal(String userName) {
+	private static void createGoal(String userName) {
         System.out.println("Enter the type of goal (PUSHUPS, PULLUPS, SQUATS, RUN):");
         String goalTypeInput = scanner.nextLine().toUpperCase();
         program.main.FitnessTracker.FitnessTracker.GoalType goalType = FitnessTracker.GoalType.valueOf(goalTypeInput.toUpperCase());
@@ -142,4 +167,21 @@ public class Main {
 
         tracker.saveData();
     }
+    
+    private static void trackActivity(String userName) {
+		
+	}
+    
+    private static void generateSummary(String userName) {
+    	
+    }
+    
+    private static void caloriesBurned(String userName) {
+    	
+    }
+    
+    private static void waterIntake(String usserName) {
+    	
+    }
+    
 }
